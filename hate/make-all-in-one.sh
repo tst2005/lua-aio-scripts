f6657f5caf44339507af8bfd283b652f101e231c
TsT <tst2005@gmail.com>
2015-09-12 21:24:02 +0200

    hate updated

--- hate/make-all-in-one.sh
+++ hate/make-all-in-one.sh
@@ -1,45 +1,49 @@
 #!/bin/sh
 
+cd -- "$(dirname "$0")" || exit 1
+
 # see https://github.com/tst2005/lua-aio
 # wget https://raw.githubusercontent.com/tst2005/lua-aio/aio.lua
-# wget https://raw.githubusercontent.com/tst2005/lua-aio/aio-cli
-ALLINONE=./aio-cli
 
 for mode in lua raw raw2; do
 
-"$ALLINONE" \
---shebang init.lua \
---mode "$mode" \
---mod hate.graphics			hate/graphics.lua \
---mod hate.opengl			hate/opengl.lua \
---mod hate.window			hate/window.lua \
---mod hate.system			hate/system.lua \
---mod hate.filesystem			hate/filesystem.lua \
---mod hate.sdl2.defines			hate/sdl2/defines.lua \
---mod hate.sdl2.cdefs			hate/sdl2/cdefs.lua \
---mod hate.sdl2.init			hate/sdl2/init.lua \
---mod hate.cpml.modules.intersect	hate/cpml/modules/intersect.lua \
---mod hate.cpml.modules.vec3		hate/cpml/modules/vec3.lua \
---mod hate.cpml.modules.constants	hate/cpml/modules/constants.lua \
---mod hate.cpml.modules.simplex		hate/cpml/modules/simplex.lua \
---mod hate.cpml.modules.quat		hate/cpml/modules/quat.lua \
---mod hate.cpml.modules.color		hate/cpml/modules/color.lua \
---mod hate.cpml.modules.mat4		hate/cpml/modules/mat4.lua \
---mod hate.cpml.modules.vec2		hate/cpml/modules/vec2.lua \
---mod hate.cpml.modules.mesh		hate/cpml/modules/mesh.lua \
---mod hate.cpml.modules.utils		hate/cpml/modules/utils.lua \
---mod hate.cpml.init			hate/cpml/init.lua \
---mod hate.timer			hate/timer.lua \
---mod hate.physfs			hate/physfs.lua \
---mod hate.math				hate/math.lua \
---mod hate.init				hate/init.lua \
---finish \
---autoaliases \
---code init.lua \
---finish \
-> "./hate-allinone-$mode.lua"
+LUA_PATH="./?.lua;./thirdparty/lua-?/?.lua;../lua-aio/?.lua;;" \
+lua -e '
+local aio = require "aio"
+aio.mode("'"$mode"'")
+aio.shebang(			"init.lua")
+local _ = aio.shellcode(			"init.lua") or aio.shellcode("hate/init.lua")
+
+aio.mod("hate.graphics", "./hate/graphics.lua")
+aio.mod("hate.opengl", "./hate/opengl.lua")
+aio.mod("hate.window", "./hate/window.lua")
+aio.mod("hate.system", "./hate/system.lua")
+aio.mod("hate.filesystem", "./hate/filesystem.lua")
+aio.mod("hate.sdl2.defines", "./hate/sdl2/defines.lua")
+aio.mod("hate.sdl2.cdefs", "./hate/sdl2/cdefs.lua")
+aio.mod("hate.sdl2.init", "./hate/sdl2/init.lua")
+aio.mod("hate.cpml.modules.intersect", "./hate/cpml/modules/intersect.lua")
+aio.mod("hate.cpml.modules.vec3", "./hate/cpml/modules/vec3.lua")
+aio.mod("hate.cpml.modules.constants", "./hate/cpml/modules/constants.lua")
+aio.mod("hate.cpml.modules.simplex", "./hate/cpml/modules/simplex.lua")
+aio.mod("hate.cpml.modules.quat", "./hate/cpml/modules/quat.lua")
+aio.mod("hate.cpml.modules.color", "./hate/cpml/modules/color.lua")
+aio.mod("hate.cpml.modules.mat4", "./hate/cpml/modules/mat4.lua")
+aio.mod("hate.cpml.modules.quadtree", "./hate/cpml/modules/quadtree.lua") -- ?
+aio.mod("hate.cpml.modules.vec2", "./hate/cpml/modules/vec2.lua")
+aio.mod("hate.cpml.modules.mesh", "./hate/cpml/modules/mesh.lua")
+aio.mod("hate.cpml.modules.octree", "./hate/cpml/modules/octree.lua") -- ?
+aio.mod("hate.cpml.modules.utils", "./hate/cpml/modules/utils.lua")
+aio.mod("hate.cpml.init", "./hate/cpml/init.lua")
+aio.mod("hate.timer", "./hate/timer.lua")
+aio.mod("hate.physfs", "./hate/physfs.lua")
+aio.mod("hate.math", "./hate/math.lua")
+aio.mod("hate.init", "./hate/init.lua")
+aio.finish()
+aio.autoaliases()
+aio.code("init.lua")
+aio.finish()
+' > "./allinone/hate-allinone-$mode.lua"
 
 done
-
-#"$ALLINONE" --shebang init.lua $( find hate/ -depth -name '*.lua' |while read -r line; do echo "--mod $(echo "$line" | sed 's,\.lua$,,g' | tr / .) ) --code init.lua
 
