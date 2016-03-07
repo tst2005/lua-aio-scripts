caad2fea1e177c16e681938e2e09fa4d8887ecd5
TsT <tst2005@gmail.com>
2016-03-07 19:19:32 +0100

    penlight script updated

--- penlight/make-all-in-one.sh
+++ penlight/make-all-in-one.sh
@@ -7,17 +7,25 @@ cd -- "$(dirname "$0")" || exit 1
 
 LUA_PATH="./?.lua;./thirdparty/lua-?/?.lua;;" \
 lua -e '
-pcall(require, "mom")
+--pcall(require, "mom")
 local aio = require "aio"
-
 aio.mode("raw2")
 
--- aio.shebang(			"bin/featuredlua")
--- aio.shellcode(		"bin/featuredlua")
-
-aio.mod("pl.xxxxx", ".../pl/...")
-
-aio.finish()
+aio.rock.auto("penlight-scm-1.rockspec", "pl")
 ' > pl.lua
 
+
+# done with:
+#	git clone https://github.com/stevedonovan/Penlight
+#	cd Penlight
+#	mkdir thirdparty
+#	cd thirdparty
+#	git clone https://github.com/tst2005/lua-aio
+#	git clone https://github.com/tst2005/lua-aio-scripts
+#	git clone https://github.com/tst2005/lua-compat-env
+#	ln -s lua-compat-env/lua lua-compat_env
+#	cd ..
+#	ln -s thirdparty/lua-aio-scripts/penlight/make-all-in-one.sh make-all-in-one.sh
+#	chmod +x make-all-in-one.sh
+#	./make-all-in-one.sh
 
