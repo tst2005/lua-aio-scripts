6d97a211ae63c5a26550bc85031ac86896d48b14
TsT <tst2005@gmail.com>
2017-09-12 17:16:03 +0200

    uncommited changes for lockbox

--- lockbox/make-all-in-one.sh
+++ lockbox/make-all-in-one.sh
@@ -5,23 +5,19 @@ cd -- "$(dirname "$0")" || exit 1
 # see https://github.com/tst2005/lua-aio
 # wget https://raw.githubusercontent.com/tst2005/lua-aio/aio.lua
 
+[ -d aio ] || mkdir aio
+
 LUA_PATH="./?.lua;../lua-?/?.lua;thirdparty/git/tst2005/lua-?/?.lua;;" \
 lua -e '
 local aio = require "aio"
 aio.mode("raw2")
 
-aio.rock.auto("rockspecs/lunajson-1.1-0.rockspec", "lunajson")
---[[
-aio.shebang(				"src/lunajson.lua")
-aio.mod("lunajson._str_lib",		"src/lunajson/_str_lib.lua")
-aio.rawmod("lunajson._str_lib_lua53", 	"src/lunajson/_str_lib_lua53.lua")
-aio.mod("lunajson.sax", 		"src/lunajson/sax.lua")
-aio.mod("lunajson.decoder",		"src/lunajson/decoder.lua")
-aio.mod("lunajson.encoder",		"src/lunajson/encoder.lua")
-aio.finish()
+aio.rock.auto("rockspecs/lockbox-0.1.0-0.rockspec", "lockbox")
+' > aio/lockbox.lua
 
-aio.code(				"src/lunajson.lua")
-aio.finish()
-]]--
-' > lunajson.lua
+[ -d aio/sha1sum ] || mkdir aio/sha1sum
+LUA_PATH="./?.lua;../lua-?/?.lua;thirdparty/git/tst2005/lua-?/?.lua;;" \
+lua -e '
+require "aio".rock.auto("rockspecs-for-aio/sha1sum-minimaldeps.rockspec", "sha1sum")
+' > aio/sha1sum/sha1sum.lua
 
