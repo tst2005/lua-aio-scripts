1f22b81fe7abf784141293d0d80a211470b860d3
TsT <tst2005@gmail.com>
2017-09-21 11:43:58 +0200

    ljsyscall bundle updated

--- ljsyscall/make-all-in-one.sh
+++ ljsyscall/make-all-in-one.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
 
-cd -- "$(dirname "$0")" || exit 1
+# see https://github.com/tst2005/lua-aio-script
 
-# see https://github.com/tst2005/lua-aio
-# wget https://raw.githubusercontent.com/tst2005/lua-aio/aio.lua
+# mkdir tmp
+# cd tmp
+
+# git clone https://github.com/tst2005/ljsyscall
+# cd ljsyscall
+
+# git clone https://github.com/tst2005/lua-aio
+# git clone https://github.com/tst2005/lua-aio-scripts
+
+# ( ln -s lua-aio/generated-bundle/aio.lua aio.lua )
+# ln -s lua-aio-scripts/lockbox bundle-setup
+
+# sh bundle-setup/make-all-in-one.sh
+# LUA=lua sh bundle-setup/make-all-in-one.sh
+# LUA=luajit2.1 sh bundle-setup/make-all-in-one.sh
 
 [ -d generated-bundle ] || mkdir generated-bundle
 
-LUA_PATH="./?.lua;../lua-?/?.lua;thirdparty/git/tst2005/lua-?/?.lua;;" \
-lua -e '
+LUA_PATH="./?.lua;./?/init.lua;./lua-?/generated-bundle/?.lua;;"
+
+[ -d generated-bundle/linux-only ] || mkdir generated-bundle/linux-only
+LUA_PATH="$LUA_PATH" \
+${LUA:-lua} -e '
 local aio = require "aio"
 aio.mode("raw2")
-
-aio.rock.auto("rockspec/ljsyscall-0.12-1.rockspec-for-bundle-linux", "syscall")
-' > generated-bundle/syscall.lua
+aio.rock.auto("bundle-setup/rockspecs-for-aio/ljsyscall-0.12-1.rockspec-for-linux-only-bundle", "syscall")
+' > generated-bundle/linux-only/syscall.lua
 
