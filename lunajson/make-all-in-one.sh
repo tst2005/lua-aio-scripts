e35205529c5553573c55510d42a12c9d7a641b94
TsT <tst2005@gmail.com>
2017-09-21 11:54:02 +0200

    lunajson updated

--- lunajson/make-all-in-one.sh
+++ lunajson/make-all-in-one.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
 
-cd -- "$(dirname "$0")" || exit 1
+# see https://github.com/tst2005/lua-aio-scripts
 
-# see https://github.com/tst2005/lua-aio
-# wget https://raw.githubusercontent.com/tst2005/lua-aio/aio.lua
+# mkdir tmp
+# cd tmp
 
-LUA_PATH="./?.lua;../lua-?/?.lua;thirdparty/git/tst2005/lua-?/?.lua;;" \
-lua -e '
-local aio = require "aio"
---aio.mode("raw2")
+# git clone https://github.com/tst2005/lunajson
+# cd lunajson
+# git clone https://github.com/tst2005/lua-aio
+# git clone https://github.com/tst2005/lua-aio-scripts
+# ln -s lua-aio-scripts/lunajson bundle-setup
 
-aio.rock.auto("rockspecs/lunajson-1.1-0.rockspec", "lunajson")
---[[
-aio.shebang(				"src/lunajson.lua")
-aio.mod("lunajson._str_lib",		"src/lunajson/_str_lib.lua")
-aio.rawmod("lunajson._str_lib_lua53", 	"src/lunajson/_str_lib_lua53.lua")
-aio.mod("lunajson.sax", 		"src/lunajson/sax.lua")
-aio.mod("lunajson.decoder",		"src/lunajson/decoder.lua")
-aio.mod("lunajson.encoder",		"src/lunajson/encoder.lua")
-aio.finish()
+# sh bundle-setup/make-all-in-one.sh
+# LUA=lua sh bundle-setup/make-all-in-one.sh
+# LUA=luajit2.1 sh bundle-setup/make-all-in-one.sh
 
-aio.code(				"src/lunajson.lua")
-aio.finish()
-]]--
-' > lunajson.lua
+[ -d generated-bundle ] || mkdir generated-bundle
+
+LUA_PATH="./?.lua;./?/init.lua;./lua-?/generated-bundle/?.lua;;"
+
+LUA_PATH="$LUA_PATH" \
+${LUA:-lua} -e '
+require"aio".rock.auto("rockspecs/lunajson-1.1-0.rockspec", "lunajson")
+' > generated-bundle/lunajson.lua
 
