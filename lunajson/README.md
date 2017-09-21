2bcdc7d1dc4d9db54d1175b2409c0d7d933778b6
TsT <tst2005@gmail.com>
2017-09-21 11:57:32 +0200

    instruction into README.md

---
+++ lunajson/README.md
@@ -0,0 +1,15 @@
+
+```sh
+mkdir tmp
+cd tmp
+
+git clone https://github.com/tst2005/lunajson
+cd lunajson
+git clone https://github.com/tst2005/lua-aio
+git clone https://github.com/tst2005/lua-aio-scripts
+ln -s lua-aio-scripts/lunajson bundle-setup
+
+sh bundle-setup/make-all-in-one.sh
+LUA=lua sh bundle-setup/make-all-in-one.sh
+LUA=luajit2.1 sh bundle-setup/make-all-in-one.sh
+```
--- lunajson/make-all-in-one.sh
+++ lunajson/make-all-in-one.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-# see https://github.com/tst2005/lua-aio-scripts
-
-# mkdir tmp
-# cd tmp
-
-# git clone https://github.com/tst2005/lunajson
-# cd lunajson
-# git clone https://github.com/tst2005/lua-aio
-# git clone https://github.com/tst2005/lua-aio-scripts
-# ln -s lua-aio-scripts/lunajson bundle-setup
-
-# sh bundle-setup/make-all-in-one.sh
-# LUA=lua sh bundle-setup/make-all-in-one.sh
-# LUA=luajit2.1 sh bundle-setup/make-all-in-one.sh
-
 [ -d generated-bundle ] || mkdir generated-bundle
 
 LUA_PATH="./?.lua;./?/init.lua;./lua-?/generated-bundle/?.lua;;"
