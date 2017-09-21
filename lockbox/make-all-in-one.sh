314972d492478b3401de9f9a4f8c05ba95226b11
TsT <tst2005@gmail.com>
2017-09-21 11:30:28 +0200

    easier bundle way

--- lockbox/make-all-in-one.sh
+++ lockbox/make-all-in-one.sh
@@ -5,13 +5,14 @@
 # mkdir tmp
 # cd tmp
 
+# git clone https://github.com/tst2005/lua-lockbox
+# cd lua-lockbox
+
 # git clone https://github.com/tst2005/lua-aio
 # git clone https://github.com/tst2005/lua-aio-scripts
 
-# git clone https://github.com/tst2005/lua-lockbox
-# cd lua-lockbox
-# ln -s ../lua-aio/generated-bundle/aio.lua aio.lua
-# ln -s ../lua-aio-scripts/lockbox bundle-setup
+# ( ln -s lua-aio/generated-bundle/aio.lua aio.lua )
+# ln -s lua-aio-scripts/lockbox bundle-setup
 
 # sh bundle-setup/make-all-in-one.sh
 # LUA=lua sh bundle-setup/make-all-in-one.sh
@@ -19,22 +20,23 @@
 
 [ -d generated-bundle ] || mkdir generated-bundle
 
-LUA_PATH="./?.lua;./?/init.lua;../lua-?/?.lua;thirdparty/git/tst2005/lua-?/?.lua;;" \
+LUA_PATH="./?.lua;./?/init.lua;./lua-?/generated-bundle/?.lua;;"
+
+LUA_PATH="$LUA_PATH" \
 ${LUA:-lua} -e '
 local aio = require "aio"
 aio.mode("raw2")
-
 aio.rock.auto("rockspecs/lockbox-0.1.0-0.rockspec", "lockbox")
 ' > generated-bundle/lockbox.lua
 
 [ -d generated-bundle/sha1sum ] || mkdir generated-bundle/sha1sum
-LUA_PATH="./?.lua;../lua-?/?.lua;thirdparty/git/tst2005/lua-?/?.lua;;" \
+LUA_PATH="$LUA_PATH" \
 ${LUA:-lua} -e '
 require "aio".rock.auto("bundle-setup/rockspecs-for-aio/sha1sum-minimaldeps.rockspec", "sha1sum")
 ' > generated-bundle/sha1sum/sha1sum.lua
 
 [ -d generated-bundle/sha256sum ] || mkdir generated-bundle/sha256sum
-LUA_PATH="./?.lua;../lua-?/?.lua;thirdparty/git/tst2005/lua-?/?.lua;;" \
+LUA_PATH="$LUA_PATH" \
 ${LUA:-lua} -e '
 require "aio".rock.auto("bundle-setup/rockspecs-for-aio/sha256sum-minimaldeps.rockspec", "sha256sum")
 ' > generated-bundle/sha256sum/sha256sum.lua
